83519005db004bbd6cb4e4fd949c306a2c94fccc
Daniel Tigges <danieltigges@googlemail.com>
2025-09-28 18:08:28 +0200

    Update

--- run_setup.sh
+++ run_setup.sh
@@ -6,7 +6,7 @@ set -eEo pipefail
 export DOTFILES_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install
-#source "$DOTFILES_PATH/packages/all.sh"
-#source "$DOTFILES_PATH/webapps/all.sh"
-#source "$DOTFILES_PATH/configurations/all.sh"
+source "$DOTFILES_PATH/packages/all.sh"
+source "$DOTFILES_PATH/webapps/all.sh"
+source "$DOTFILES_PATH/configurations/all.sh"
 source "$DOTFILES_PATH/dotfiles/all.sh"
