4b863dd9971f216b25c984a9eb805e461caed589
Daniel Tigges <danieltigges@googlemail.com>
2026-01-18 15:21:56 +0100

    Update

---
+++ dotfiles/waybar/.config/waybar/vpn/toggle-vpn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source "$(dirname "$0")/vpn.conf"
+if ip link show | grep -q "$VPN_NAME"; then
+  echo "$(date -u +'%Y-%m-%dT%H:%M:%SZ') - VPN DOWN" >>"$(dirname $0)/vpn.log"
+  sudo wg-quick down "$VPN_NAME"
+else
+  echo "$(date -u +'%Y-%m-%dT%H:%M:%SZ') - VPN UP $VPN_NAME" >>"$(dirname $0)/vpn.log"
+  resolvconf -u
+  wg-quick up "$VPN_NAME"
+fi
