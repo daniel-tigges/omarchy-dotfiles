aca81455bf8bda6dcc5524ff6681107dedd0d3f5
Daniel Tigges <danieltigges@googlemail.com>
2026-05-09 21:35:47 +0200

    Disable Lid close

--- configurations/all.sh
+++ configurations/all.sh
@@ -1,5 +1,5 @@
 source "$DOTFILES_PATH/configurations/register-systemd-services.sh"
 source "$DOTFILES_PATH/configurations/disable-bluetooth-idle-timeout.sh"
 source "$DOTFILES_PATH/configurations/enable-wake-on-usb-keyboard.sh"
-source "$DOTFILES_PATH/configurations/disable-lid-closed-events.sh"
+#source "$DOTFILES_PATH/configurations/disable-lid-closed-events.sh"
 source "$DOTFILES_PATH/configurations/set-power-button-behavior.sh"
