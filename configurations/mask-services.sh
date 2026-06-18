0d91f3bcd8424bddaf5b778f534da2c6ef1d195e
Daniel Tigges <danieltigges@googlemail.com>
2026-06-18 21:46:54 +0200

    Fix display and lid switch misbehavior

--- configurations/mask-services.sh
+++ configurations/mask-services.sh
@@ -3,4 +3,8 @@
 
 # Some systemd Service that causes the boot process to fail, so we need to disable them
 sudo systemctl mask systemd-pcrproduct.service
+
+# configure libvirt
+sudo systemctl enable --now libvirtd
+sudo usermod -aG libvirt $USER
 echo " → Masked systemd-pcrproduct.service"
